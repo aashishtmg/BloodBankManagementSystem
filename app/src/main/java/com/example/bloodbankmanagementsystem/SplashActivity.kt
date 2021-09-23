8eee14f88dcba092a5fd0df7b79f04e1aed7b949
aashishtmg <syangtanaashish7@gmail.com>
2021-09-23 16:17:46 +0545

    splash screen

---
+++ app/src/main/java/com/example/bloodbankmanagementsystem/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.bloodbankmanagementsystem
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    var currentProgess : Int = 1000
+    private lateinit var progressBar : ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        progressBar = findViewById(R.id.progressbar)
+
+        progressBar.max = 1000
+
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgess)
+            .setDuration(5000)
+            .start()
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(5000)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+
+
+            startActivity(intent)
+            finish()
+        }
+    }
+}
