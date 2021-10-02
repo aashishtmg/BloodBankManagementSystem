eee1de2dee96989961bd25f3c3eef19e8590e88c
aashishtmg <syangtanaashish7@gmail.com>
2021-10-02 13:06:53 +0545

    BDD login and signup testing

---
+++ app/src/androidTest/java/com/example/bloodbankmanagementsystem/LoginTesting.kt
@@ -0,0 +1,44 @@
+package com.example.bloodbankmanagementsystem
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@LargeTest
+@RunWith(JUnit4::class)
+class LoginTesting {
+    @get:Rule
+    val testRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun checkArithmetic() {
+        onView(withId(R.id.email))
+            .perform(typeText("aashish@gmail.com"))
+
+        onView(withId(R.id.password))
+            .perform((typeText("aashish")))
+
+        closeSoftKeyboard()
+
+        onView(withId(R.id.login))
+            .perform(click())
+
+        Thread.sleep(2000)
+
+        onView(withId(R.id.tablayout))
+            .check(matches(ViewMatchers.isDisplayed()))
+    }
+}
---
+++ app/src/androidTest/java/com/example/bloodbankmanagementsystem/RegisterUITest.kt
@@ -0,0 +1,47 @@
+package com.example.bloodbankmanagementsystem
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@LargeTest
+@RunWith(JUnit4::class)
+class RegisterUITest {
+    @get:Rule
+    val testRule = ActivityScenarioRule(SignupActivity::class.java)
+
+    @Test
+    fun testSignupUI() {
+        onView(withId(R.id.edfname))
+            .perform(typeText("Bikash"))
+        onView(withId(R.id.edlname))
+            .perform(typeText("Thapa"))
+        onView(withId(R.id.edusername))
+            .perform(typeText("bikash123"))
+        onView(withId(R.id.edaddress))
+            .perform(typeText("padampur"))
+        onView(withId(R.id.edemail))
+            .perform(typeText("bikash@gmail.com"))
+        onView(withId(R.id.edphone))
+            .perform(typeText("9865036355"))
+        onView(withId(R.id.edpassword))
+            .perform(scrollTo(), ViewActions.click())
+            .perform(typeText("bikash"))
+        onView(withId(R.id.confirmpassword))
+            .perform(scrollTo(), ViewActions.click())
+            .perform(typeText("bikash"))
+        closeSoftKeyboard()
+        onView(withId(R.id.btnsignup)).perform(scrollTo(), ViewActions.click())
+            .perform(ViewActions.click())
+    }
+}
